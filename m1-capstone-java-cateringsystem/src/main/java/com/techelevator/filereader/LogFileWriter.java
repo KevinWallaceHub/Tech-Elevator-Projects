2a5ea4561de67360685d55b562642da6a91798bd
Kevin Wallace <Kevinwallace0546@gmail.com>
2022-06-10 16:50:17 -0400

    Capstone requirments complete

--- m1-capstone-java-cateringsystem/src/main/java/com/techelevator/filereader/LogFileWriter.java
+++ m1-capstone-java-cateringsystem/src/main/java/com/techelevator/filereader/LogFileWriter.java
@@ -3,6 +3,7 @@ package com.techelevator.filereader;
 import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /*
     This class should contain any and all details of access to the log file
@@ -17,8 +18,9 @@ public class LogFileWriter {
                 PrintWriter logWriter = new PrintWriter(fileWriter)
         ) {
             LocalDateTime now = LocalDateTime.now();
+            String formatedTimeStamp = now.format(DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a"));
 
-            logWriter.println(now + " " + messageToLog);
+            logWriter.println(formatedTimeStamp + " " + messageToLog);
 
         } catch (IOException e) {
             System.out.println("Who do I tell when my logger breaks????");
@@ -28,19 +30,19 @@ public class LogFileWriter {
 
     public void logMoneyAdded(int amountAdded, float currentAccountBalance) {
 
-        logWriter("ADD MONEY: $" + amountAdded + " $" + currentAccountBalance);
+        logWriter("ADD MONEY: $" +amountAdded + ".00 $" + String.format("%.2f",currentAccountBalance));
     }
 
     public void logProductOrdered(float currentAccountBalance, int quantity, String description, String productCode,
                                   float pricePaid) {
 
-        logWriter(quantity + " " + description + " " + productCode + " $" + String.format("%.2f",pricePaid) + " $" + currentAccountBalance);
+        logWriter(quantity + " " + description + " " + productCode + " $" + String.format("%.2f",pricePaid) + " $" + String.format("%.2f",currentAccountBalance));
 
     }
 
     public void logGiveChange(float amount) {
 
-        logWriter("GIVE CHANGE: $" + amount + " $0.00");
+        logWriter("GIVE CHANGE: $" + String.format("%.2f",amount) + " $0.00");
 
     }
 
